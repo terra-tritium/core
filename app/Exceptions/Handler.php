2eda9c04b925157aef200554091f5fd57d16bf81
RonielViana <ronielvb@hotmail.com>
2023-02-16 15:56:50 -0300

    Docker e token

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,31 +4,23 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use \Symfony\Component\HttpFoundation\Response;
 
 class Handler extends ExceptionHandler
 {
     /**
-     * A list of exception types with their corresponding custom log levels.
-     *
-     * @var array<class-string<\Throwable>, \Psr\Log\LogLevel::*>
-     */
-    protected $levels = [
-        //
-    ];
-
-    /**
      * A list of the exception types that are not reported.
      *
-     * @var array<int, class-string<\Throwable>>
+     * @var array
      */
     protected $dontReport = [
         //
     ];
 
     /**
-     * A list of the inputs that are never flashed to the session on validation exceptions.
+     * A list of the inputs that are never flashed for validation exceptions.
      *
-     * @var array<int, string>
+     * @var array
      */
     protected $dontFlash = [
         'current_password',
@@ -44,7 +36,72 @@ class Handler extends ExceptionHandler
     public function register()
     {
         $this->reportable(function (Throwable $e) {
-            //
+
         });
+
+        $this->renderable(function (Throwable $e) {
+            return $this->handleException($e);
+        });
+
+    }
+
+    public function handleException( Throwable $e){
+        if ($e instanceof HttpException) {
+            $code = $e->getStatusCode();
+            $defaultMessage = Response::$statusTexts[$code];
+            $message = $e->getMessage() == "" ? $defaultMessage : $e->getMessage();
+            return $this->errorResponse($message, $code);
+        } else if ($e instanceof \Illuminate\Database\Eloquent\ModelNotFoundException) {
+            $model = strtolower(class_basename($e->getModel()));
+            return $this->errorResponse("Does not exist any instance of {$model} with the given id", Response::HTTP_NOT_FOUND);
+        } else if ($e instanceof \Illuminate\Auth\AuthorizationException) {
+            return $this->errorResponse($e->getMessage(), Response::HTTP_FORBIDDEN);
+        } else if ($e instanceof TokenBlacklistedException) {
+            return $this->errorResponse($e->getMessage(), Response::HTTP_UNAUTHORIZED);
+        } else if ($e instanceof \Illuminate\Auth\AuthenticationException) {
+            return $this->errorResponse($e->getMessage(), Response::HTTP_UNAUTHORIZED);
+        } else if ($e instanceof \Illuminate\Validation\ValidationException) {
+            $errors = $e->validator->errors()->getMessages();
+            return $this->errorResponse($errors, Response::HTTP_UNPROCESSABLE_ENTITY);
+        } else {
+            if (config('app.debug'))
+            {
+                return $this->dataResponse($e->getMessage());
+            }else {
+                return $this->errorResponse('Tente novamente mais tarde', Response::HTTP_INTERNAL_SERVER_ERROR);
+            }
+        }
+    }
+
+    /**
+     * Data Response
+     * @param $data
+     * @return JsonResponse
+    */
+    public function dataResponse($data){
+        return response()->json(['message' => $data,'success'=>false,'code' =>  Response::HTTP_INTERNAL_SERVER_ERROR]);
+    }
+
+    /**
+     * Success Response
+     * @param string $message
+     * @param int $code
+     * @return JsonResponse
+     */
+    public function successResponse(string $message, $code = Response::HTTP_OK)
+    {
+        return response()->json(['message' => $message, 'code' => $code], $code);
+    }
+
+    /**
+     * Error Response
+     * @param $message
+     * @param int $code
+     * @return JsonResponse
+     *
+     */
+    public function errorResponse($message, $code = Response::HTTP_BAD_REQUEST)
+    {
+        return response()->json(['message' => $message,'success'=>false, 'code' => $code], $code);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\BuildController;
 use App\Http\Controllers\CountryController;
 use App\Http\Controllers\PlanetController;
 use App\Http\Controllers\PlayerController;
+use App\Http\Controllers\AuthController;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -23,25 +24,34 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::controller(BuildController::class)->group(function () {
-    Route::get('/build/list', 'list');
-    Route::get('/build/availables/{planet}', 'availables');
-    Route::get('/building/list/{planet}', 'listBildings');
-    Route::post('/build/plant', 'plant');
-    Route::post('/build/up', 'upgrade');
-    Route::post('/build/workers', 'workers');
-    Route::get('/build/requires/{build}', 'requires');
+Route::controller(AuthController::class)->group(function () {
+    Route::post('/create-token','createToken');
 });
 
-Route::controller(CountryController::class)->group(function () {
-    Route::get('/country/list', 'list');
-});
 
-Route::controller(PlanetController::class)->group(function () {
-    Route::get('/planet/list/{address}', 'list');
-});
+Route::middleware('auth:sanctum')->group(function () {  
 
-Route::controller(PlayerController::class)->group(function () {
-    Route::get('/player/show/{address}', 'show');
-    Route::post('/player/register', 'register');
+    Route::controller(BuildController::class)->group(function () {
+        Route::get('/build/list', 'list');
+        Route::get('/build/availables/{planet}', 'availables');
+        Route::get('/building/list/{planet}', 'listBildings');
+        Route::post('/build/plant', 'plant');
+        Route::post('/build/up', 'upgrade');
+        Route::post('/build/workers', 'workers');
+        Route::get('/build/requires/{build}', 'requires');
+    });
+
+    Route::controller(CountryController::class)->group(function () {
+        Route::get('/country/list', 'list');
+    });
+
+    Route::controller(PlanetController::class)->group(function () {
+        Route::get('/planet/list/{address}', 'list');
+    });
+
+    Route::controller(PlayerController::class)->group(function () {
+        Route::get('/player/show/{address}', 'show');
+        Route::post('/player/register', 'register');
+    });
+
 });
