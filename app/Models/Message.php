9bb02ac0925cd35b30ff3d87ee682a29a17a0316
Kleiton Batista <kleiton.a.batista@gmail.com>
2023-04-16 19:39:25 -0300

    Rota inicial de mensagens

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
 
 class Message extends Model
 {
@@ -18,4 +20,19 @@ class Message extends Model
         'status',
         'read'
     ];
+
+    public function getAll(){
+        $msgs = DB::table($this->table)->get();
+        return $msgs;
+    }
+
+    public function getAllByUserSender($senderId){
+        $msg = DB::table($this->table)->where('senderId',$senderId)->get();
+        return $msg;
+    }
+    
+    public function getMsg(){
+        $msg = DB::table($this->table)->where('recipientId','3')->get();
+        return $msg;
+    }
 }
