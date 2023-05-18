77d800514083a771b6d17ece76a05984624a3065
Larissa Sá <oli.larissa@gmail.com>
2023-05-18 01:24:53 -0300

    desfazendo add relacionamento de aliança na tabela ranking

--- database/migrations/2023_02_13_174803_start.php
+++ database/migrations/2023_02_13_174803_start.php
@@ -106,7 +106,6 @@ return new class extends Migration
             $table->id();
             $table->string("name");
             $table->integer('player')->constrained("players");
-            $table->foreignId('aliance')->constrained("aliances");
             $table->bigInteger("energy");
             $table->bigInteger("score");
             $table->bigInteger("buildScore");
