64682be713fecd47552ac5603f93f4efe1e93d76
Kleiton Batista <kleiton.a.batista@gmail.com>
2023-06-08 17:05:01 -0300

    Recuperando os dados da trading
    Recuperando os dados da trading, filtrando pelo mercado da regiao do usuario logado e pelo tipo de item negociado

--- app/Models/Trading.php
+++ app/Models/Trading.php
@@ -24,5 +24,25 @@ class Trading extends Model
         'updatedAt',
     ];
 
+    public function getDadosTradingByResourceAndMarket($type, $region){
+        $trading = DB::table($this->table . ' as t')->select("t.*","p.name")
+                ->join('market as m', 'm.id', '=', 't.idMarket')
+                ->join('planets as planeta', 'planeta.id', '=', 't.idPlanetCreator')
+                ->join('players as p', 'p.id', '=', 'planeta.id')
+                 ->where('m.status', true)
+                 ->where('t.status', true)
+                ->where('m.region', '=', $region)
+                ->where('t.resource','=', $type)
+                ->get();
+         return $trading;       
+    }
+    /*
+    $msgs = DB::table($this->table . ' as m')
+            ->select('us.name as sender', 'ur.name as recipient', 'm.*')
+            ->join('users as us', 'us.id', '=', 'm.senderId')
+            ->join('users as ur', 'ur.id', '=', 'm.recipientId')
+            ->get();
+        return $msgs;
+    */
     
 }
