4aa61c5f875e1ed97028b005ff83ed95586e7881
Marcelo Rocha <marcelobentodarocha@gmail.com>
2023-09-01 10:47:30 -0300

    feat: :sparkles: adicionado mapeamento das constantes
    feito o mapeamento das contates e relacionamento dos models

--- app/Models/Planet.php
+++ app/Models/Planet.php
@@ -4,11 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Planet extends Model
 {
     use HasFactory;
     protected $table = 'planets';
     public $timestamps = false;
-    
+
+    /**
+     * Return all builds
+     *
+     * @return HasToMany
+     */
+    public function buildings(): HasMany
+    {
+        return $this->hasMany(
+            Building::class,
+            'planet',
+            'id'
+        );
+    }
 }
