a16ac353f534c12ea97be47fb4e780258443ac9f
Fábio Rodrigues <binho_mal@hotmail.com>
2023-10-02 18:26:13 -0300

    Return the original code

--- app/Services/ResearchService.php
+++ app/Services/ResearchService.php
@@ -21,7 +21,7 @@ class ResearchService
 
     public function laboratoryConfig($player, $planet, $power)
     {
-        // 1. Verificações iniciais
+
         $planet->workersWaiting = $planet->workers - ($planet->workersOnMetal + $planet->workersOnUranium + $planet->workersOnCrystal + $power);
 
         # Don't have enough workers
@@ -29,162 +29,138 @@ class ResearchService
             return false;
         }
 
-        // 2. Inicie a transação do banco de dados
-        return DB::transaction(function () use ($player, $planet, $power) {
-            $this->playerSincronize($player);
-            $planet->pwResearch = $power;
-            $planet->workersOnLaboratory = (int) $power;
-            $planet->save();
-
-            $this->workerService->syncronizeEnergy($planet);
-            $this->updateBuildPower($planet->id, $power);
-
-            return ['success' => true, 'planet' => $planet];
-        });
-    }
-
-    public function buyResearch($player, $research)
-    {
-        // 1. Sincronize o jogador primeiro
         $this->playerSincronize($player);
+        $planet->pwResearch = $power;
+        $planet->workersOnLaboratory = (int) $power;
+        $planet->save();
 
-        // 2. Verificações iniciais
-        // Verifique se o jogador tem RP suficientes
-        if ($player->researchPoints < $research->cost) {
-            return false;
-        }
+        $this->workerService->syncronizeEnergy($planet);
+        $this->updateBuildPower($planet->id, $power);
 
-        // Verifique se a pesquisa já foi realizada
+        return $planet;
+    }
+
+    public function buyResearch ($player, $research) {
+        $this->playerSincronize($player);
+        $researched = new Researched();
+        $researched->player = $player->id;
+        $researched->code = $research->code;
+        $player->researchPoints -= $research->cost;
+        
+        # Don't have enough balance
+        if ($player->researchPoints < 0) { return false; }
+
         $existsResearch = Researched::where([['player', $player->id], ['code', $research->code]])->first();
-        if ($existsResearch) {
-            return false;
+
+        # Already researched
+        if ($existsResearch) { return false; }
+
+        # Hyperspeed
+        if ($research->code == 600) {
+            $this->bonusService->addSpeedTravel($player, 1);
         }
 
-        // 3. Inicie a transação do banco de dados
-        return DB::transaction(function () use ($player, $research) {
-            // Atualize os RP do jogador
-            $player->researchPoints -= $research->cost;
+        # Future War 1
+        if ($research->code == 1000) {
+            $this->bonusService->addSpeedProduceUnit($player, 1);
+        }
 
-            // Registre a nova pesquisa
-            $researched = new Researched();
-            $researched->player = $player->id;
-            $researched->code = $research->code;
-            $researched->save();
+        # Future War 2
+        if ($research->code == 1100) {
+            $this->bonusService->addSpeedProduceUnit($player, 1);
+        }
 
-            # Hyperspeed
-            if ($research->code == 600) {
-                $this->bonusService->addSpeedTravel($player, 1);
-            }
+        # Future War 3
+        if ($research->code == 1200) {
+            $this->bonusService->addSpeedProduceUnit($player, 1);
+        }
 
-            # Future War 1
-            if ($research->code == 1000) {
-                $this->bonusService->addSpeedProduceUnit($player, 1);
-            }
+        # Energy Renewable
+        if ($research->code == 1600) {
+            $this->bonusService->addDiscountEnergy($player, 1);
+        }
 
-            # Future War 2
-            if ($research->code == 1100) {
-                $this->bonusService->addSpeedProduceUnit($player, 1);
-            }
+        # Factory
+        if ($research->code == 1900) {
+            $this->bonusService->addDiscountHumanoid($player, 1);
+        }
 
-            # Future War 3
-            if ($research->code == 1200) {
-                $this->bonusService->addSpeedProduceUnit($player, 1);
-            }
+        # Power Supply
+        if ($research->code == 2100) {
+            $this->bonusService->addDiscountEnergy($player, 1);
+        }
 
-            # Energy Renewable
-            if ($research->code == 1600) {
-                $this->bonusService->addDiscountEnergy($player, 1);
-            }
+        # Futere Economy 1
+        if ($research->code == 2300) {
+            $this->bonusService->addSpeedMining($player, 1);
+        }
 
-            # Factory
-            if ($research->code == 1900) {
-                $this->bonusService->addDiscountHumanoid($player, 1);
-            }
+        # Futere Economy 2
+        if ($research->code == 2400) {
+            $this->bonusService->addSpeedMining($player, 1);
+        }
 
-            # Power Supply
-            if ($research->code == 2100) {
-                $this->bonusService->addDiscountEnergy($player, 1);
-            }
+        # Futere Economy 3
+        if ($research->code == 2500) {
+            $this->bonusService->addSpeedMining($player, 1);
+        }
 
-            # Futere Economy 1
-            if ($research->code == 2300) {
-                $this->bonusService->addSpeedMining($player, 1);
-            }
+        # Ligth Reflexion
+        if ($research->code == 2800) {
+            $this->bonusService->addDiscountEnergy($player, 1);
+        }
 
-            # Futere Economy 2
-            if ($research->code == 2400) {
-                $this->bonusService->addSpeedMining($player, 1);
-            }
+        # Locator
+        if ($research->code == 3300) {
+            $this->bonusService->addSpeedTravel($player, 1);
+        }
 
-            # Futere Economy 3
-            if ($research->code == 2500) {
-                $this->bonusService->addSpeedMining($player, 1);
-            }
+        # Alien Technology
+        if ($research->code == 3400) {
+            $this->bonusService->addSpeedProduceShip($player, 1);
+        }
 
-            # Ligth Reflexion
-            if ($research->code == 2800) {
-                $this->bonusService->addDiscountEnergy($player, 1);
-            }
+        # Future Science 1
+        if ($research->code == 3500) {
+            $this->bonusService->addSpeedResearch($player, 1);
+        }
 
-            # Locator
-            if ($research->code == 3300) {
-                $this->bonusService->addSpeedTravel($player, 1);
-            }
+        # Future Science 2
+        if ($research->code == 3600) {
+            $this->bonusService->addSpeedResearch($player, 1);
+        }
 
-            # Alien Technology
-            if ($research->code == 3400) {
-                $this->bonusService->addSpeedProduceShip($player, 1);
-            }
+        # Future Science 3
+        if ($research->code == 3700) {
+            $this->bonusService->addSpeedResearch($player, 1);
+        }
 
-            # Future Science 1
-            if ($research->code == 3500) {
-                $this->bonusService->addSpeedResearch($player, 1);
-            }
+        $points = $research->cost * env('TRITIUM_RESEARCH_POINTS');
 
-            # Future Science 2
-            if ($research->code == 3600) {
-                $this->bonusService->addSpeedResearch($player, 1);
-            }
+        $player->score += $points;
+        $player->researchScore += $points;
 
-            # Future Science 3
-            if ($research->code == 3700) {
-                $this->bonusService->addSpeedResearch($player, 1);
-            }
-
-            $points = $research->cost * env('TRITIUM_RESEARCH_POINTS');
-
-            $player->score += $points;
-            $player->researchScore += $points;
-
-            $researched->save();
-            $player->save();
-            return $researched;
-        });
+        $researched->save();
+        $player->save();
+        return $researched;
     }
-    public function planetSincronize($planet)
-    {
-        return DB::transaction(function () use ($planet) {
-            $planet->researchPoints = $planet->pwResearch * (time() - $planet->timeResearch) * env('TRITIUM_RESEARCH_SPEED');
-            $planet->timeResearch = time();
-            $planet->save();
-            return $planet;
-        });
+    public function planetSincronize($planet) {
+        $planet->researchPoints = $planet->pwResearch * (time() - $planet->timeResearch) * env('TRITIUM_RESEARCH_SPEED');
+        $planet->timeResearch = time();
+        $planet->save();
+        return $planet;
     }
 
-    public function playerSincronize($player)
-    {
-        return DB::transaction(function () use ($player) {
-            $points = 0;
-            $planets = $this->playerService->getPlanets();
-            foreach ($planets as $planet) {
-                $p = $this->planetSincronize($planet);
-                $points += $p->researchPoints;
-            }
-            $player->researchPoints += $points;
-            $player->save();
-            return $player;
-        });
-    }
+    public function playerSincronize($player) {
+        $points = 0;
+        $planets = $this->playerService->getPlanets();
+        foreach ($planets as $planet) {
+            $p = $this->planetSincronize($planet);
+            $points += $p->researchPoints;
+        }
+        $player->researchPoints += $points;
+        $player->save();
+    }  
 
 
     public function updateBuildPower($planet, $power)
