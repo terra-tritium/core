9fb1f0d7233f2bfaf9952934226d5089e644915e
Fábio Rodrigues <binho_mal@hotmail.com>
2023-10-20 17:52:19 -0300

    Create NFT Game Mode, Expasion Research

--- database/seeders/ModeSeeder.php
+++ database/seeders/ModeSeeder.php
@@ -23,50 +23,57 @@ class ModeSeeder extends Seeder
         ]);
 
         DB::table('modes')->insert([
-            'name' => "Space Titan",
+            'name' => "Colonizer",
             'code' => 2,
+            'description' => 'Construction Cost -10% and Protection +10%',
+            'image' => 'key.png'
+        ]);
+
+        DB::table('modes')->insert([
+            'name' => "Space Titan",
+            'code' => 3,
             'description' => 'Robot Construction Speed +20% / Attack +2% / Research Speed -20% / Mining Speed -5%',
             'image' => 'spacetitan.png'
         ]);
 
         DB::table('modes')->insert([
             'name' => "Researcher",
-            'code' => 3,
+            'code' => 4,
             'description' => 'Research Speed +20% / Construction Cost +20%',
             'image' => 'researcher.png'
         ]);
 
         DB::table('modes')->insert([
             'name' => "Engineer",
-            'code' => 4,
+            'code' => 5,
             'description' => 'Ship Construction Speed +20% / Research Speed -20%',
             'image' => 'engineer.png'
         ]);
 
         DB::table('modes')->insert([
             'name' => "Protector",
-            'code' => 5,
+            'code' => 6,
             'description' => 'Protection +20%',
             'image' => 'protector.png'
         ]);
 
         DB::table('modes')->insert([
             'name' => "Builder",
-            'code' => 6,
+            'code' => 7,
             'description' => 'Construction Cost -20% / Robot and Ship Construction Speed decreases -20%',
             'image' => 'builder.png'
         ]);
 
         DB::table('modes')->insert([
             'name' => "Navigator",
-            'code' => 7,
+            'code' => 8,
             'description' => 'Travel Speed +20% / Robot and Ship Construction Speed decreases -20%',
             'image' => 'navigator.png'
         ]);
 
         DB::table('modes')->insert([
             'name' => "Miner",
-            'code' => 8,
+            'code' => 9,
             'description' => 'Mining Speed +2% / Protection -20%',
             'image' => 'miner.png'
         ]);       
