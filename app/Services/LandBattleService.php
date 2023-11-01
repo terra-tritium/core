9fae74d52a3ac5193da0c2913a5b6a607e347c55
Andre Amorim <andre.php6@gmail.com>
2023-11-01 14:34:49 -0300

    Conceitual funcitons

---
+++ app/Services/LandBattleService.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Services;
+
+class LandBattleService
+{
+  public function start () {
+
+  }
+
+  public function join () {
+
+  }
+
+  public function loadAttackTroops () {
+
+  }
+
+  public function loadDefenseTroops () {
+
+  }
+
+  public function startStage () {
+
+  }
+
+  public function calcAttackForce () {
+
+  }
+
+  public function calcDefenseForce () {
+
+  }
+
+  public function applyAttackEffects () {
+
+  }
+
+  public function applyDefenseEffects () {
+
+  }
+
+  public function resolveStrategy () {
+
+  }
+
+  public function calcDemage () {
+
+  }
+
+  public function hitShield () {
+
+  }
+
+  public function hitTroops () {
+
+  }
+
+  public function isEmptyTroops () {
+
+  }
+
+  public function isWhiteFlag () {
+
+  }
+
+  public function resolveAtack () {
+
+  }
+
+  public function resolveDefense () {
+
+  }
+
+  public function getAttackTroops () {
+
+  }
+
+  public function getDefenseTroops () {
+
+  }
+}
