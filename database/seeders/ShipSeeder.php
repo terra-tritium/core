e2c8f42ef92e6618504265f37270cda958b5c216
Andre Amorim <andre.php6@gmail.com>
2024-02-03 20:57:51 -0300

    Rename ships

--- database/seeders/ShipSeeder.php
+++ database/seeders/ShipSeeder.php
@@ -39,94 +39,94 @@ class ShipSeeder extends Seeder
 
         
         DB::table('ships')->insert([
-            "name" => "Dark Sun Troopship DST22",
-            "nick" => "Dark",
-            "description" => "Troop Transport-class Conveyors are the backbone of ground-based campaigns, ensuring your faction's dominance through strategic mobility and efficient deployment. Secure new frontiers, protect vital assets, and conquer the unknown with the dependable and adaptable Troop Transport-class Conveyors in Terra Tritium.",
+            "name" => "Fighter Craft",
+            "nick" => "Fighter",
+            "description" => "Fast and agile, fighter crafts are ideal for short-range combat. They are often used to intercept other lighter ships or for quick attacks.",
             "image" => "ship-01.png",
             "type" => "fleet",
             "defense" => 5,
             "attack" => 5,
             "life" => 100,
-            "metal" => 4000,
-            "uranium" => 500,
-            "crystal" => 300,
-            "time"=> 250
+            "metal" => 5000,
+            "uranium" => 0,
+            "crystal" => 0,
+            "time"=> 5
         ]);
  
 
         DB::table('ships')->insert([
-            "name" => "Star Camel Tradewing SCT09 ",
-            "nick" => "Star",
-            "description" => "Cargo Freighter-class Haulers are the economic backbone of your faction, facilitating trade and prosperity as you navigate the intricate web of commerce in Terra Tritium. Embark on profitable ventures, establish lucrative trade routes, and watch your faction's wealth soar with the reliable Cargo Freighter-class Haulers at the forefront of your economic endeavors.",
+            "name" => "Heavy Bomber",
+            "nick" => "Bomber",
+            "description" => "Designed to attack fortified targets or capital ships, heavy bombers carry powerful armament to destroy larger and more protected targets.",
             "image" => "ship-02.png",
             "type" => "fleet",
             "defense" => 5,
             "attack" => 5,
             "life" => 100,
-            "metal" => 3000,
-            "uranium" => 1000,
-            "crystal" => 300,
-            "time"=> 200
+            "metal" => 15000,
+            "uranium" => 0,
+            "crystal" => 0,
+            "time"=> 25
         ]);
 
         DB::table('ships')->insert([
-            "name" => "GKhan Crusier LN77",
-            "nick" => "GKhan",
-            "description" => "As the vanguard of your fleet, Strike-class Assault Cruisers are the embodiment of offensive prowess, striking fear into the hearts of your foes. Deploy them strategically, unleash their devastating firepower, and assert your faction's dominance across the cosmic battlegrounds of Terra Tritium.",
+            "name" => "Battle Cruiser",
+            "nick" => "Cruiser",
+            "description" => "Medium-sized ships, battle cruisers are versatile and can be used for both defense and offense. They usually have a balanced combination of firepower and resilience.",
             "image" => "ship-03.png",
             "type" => "fleet",
             "defense" => 5,
             "attack" => 5,
             "life" => 100,
-            "metal" => 5000,
-            "uranium" => 2500,
-            "crystal" => 300,
-            "time"=> 250
+            "metal" => 25000,
+            "uranium" => 10000,
+            "crystal" => 0,
+            "time"=> 25
         ]);
 
         DB::table('ships')->insert([
-            "name" => "Walker 07 Scout Ship",
-            "nick" => "Walker",
+            "name" => "Scout Walker 07",
+            "nick" => "Scout",
             "description" => "Swift and agile, scout-class starships are the trailblazers of the cosmos. Ideal for scouting uncharted territories, these vessels excel at reconnaissance, evasion, and gathering vital intelligence. Uncover the mysteries of the universe with these sleek and nimble explorers.",
             "image" => "ship-04.png",
             "type" => "fleet",
             "defense" => 5,
             "attack" => 5,
             "life" => 100,
-            "metal" => 2000,
-            "uranium" => 500,
-            "crystal" => 500,
-            "time"=> 100
+            "metal" => 50000,
+            "uranium" => 0,
+            "crystal" => 0,
+            "time"=> 30
         ]);
 
         DB::table('ships')->insert([
-            "name" => "Starfinder Settler ZKO10",
-            "nick" => "Starfinder",
-            "description" => "Colony Transport-class Settlers are the first chapter in the narrative of your faction's expansion, carrying the hopes and dreams of colonists to the far reaches of Terra Tritium. Command these vessels wisely, choose colony sites strategically, and witness the flourishing of new civilizations under the banner of your faction.",
+            "name" => "Stealth Ship",
+            "nick" => "Stealth",
+            "description" => "Using advanced camouflage technology, stealth ships can approach enemies without being detected. They are ideal for ambushes and surprise attacks.",
             "image" => "ship-05.png",
             "type" => "fleet",
             "defense" => 5,
             "attack" => 5,
             "life" => 100,
-            "metal" => 5000,
-            "uranium" => 2500,
-            "crystal" => 2500,
-            "time"=> 250
+            "metal" => 50000,
+            "uranium" => 10000,
+            "crystal" => 25000,
+            "time"=> 100
         ]);
 
         DB::table('ships')->insert([
-            "name" => "Missile GKN17",
-            "nick" => "Missile",
-            "description" => "warfare. Deploy them strategically, exploit the weaknesses of your adversaries, and witness the skies ablaze as these precision missiles relentlessly pursue and annihilate enemy starships in the ongoing cosmic struggle for supremacy.",
+            "name" => "Flagship",
+            "nick" => "Flagship",
+            "description" => "The flagship is the largest and most powerful in the fleet. Besides being a command platform, it often possesses massive firepower and is crucial for fleet leadership and stability.",
             "image" => "ship-06.png",
             "type" => "fleet",
             "defense" => 5,
             "attack" => 5,
             "life" => 100,
-            "metal" => 10000,
-            "uranium" => 5000,
-            "crystal" => 5000,
-            "time"=> 500
+            "metal" => 350000,
+            "uranium" => 500000,
+            "crystal" => 50000,
+            "time"=> 250
         ]);    
     }
 }
