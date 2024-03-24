cfcce2b9565ec621da0cbb44995fbf8a7a90b6ac
Roniel <ronielvb@gmail.com>
2024-03-24 08:40:45 -0300

    Outro repactchar

--- config/api.php
+++ config/api.php
@@ -4,4 +4,5 @@ return [
     'url_site' => env("ENV_URL_SITE"),
     'tritium_travel_mission_spionage_cost' => env("TRITIUM_TRAVEL_MISSION_SPIONAGE_COST"),
     'app_env'  => env("APP_ENV"),
+    'app_secret_key'  => env("NOCAPTCHA_SECRET"),
 ];
