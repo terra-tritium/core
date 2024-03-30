d0d886421b2a0db6aa1b1470ae1f69e6ced5d08f
Andre Amorim <andre.php6@gmail.com>
2024-03-30 14:38:15 -0300

    change error pass message

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -14,7 +14,7 @@ class UserService
         $result = new LoginResult();
         $result->success = false;
         $result->verified_email = true;
-        $result->message  = 'Invalid Credentials';
+        $result->message  = 'Username or password incorrect. Please try again.';
 
         if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){
             $user   = Auth::user();
