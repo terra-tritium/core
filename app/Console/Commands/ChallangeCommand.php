758a2201cfbe9609a4617e47dde513424a10b050
Andre Amorim <andre.php6@gmail.com>
2024-04-07 12:04:39 -0300

    Include Yello Trit

---
+++ app/Console/Commands/ChallangeCommand.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
+class ChallangeCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'challange:start';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Updated new Tritium Challange';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        try {
+            
+            //DB::table('planets')->insert("");
+
+        } catch (\Exception $exception) {
+            Log::error('Erro ao executar starto do tritium challange: ' . $exception->getMessage());
+        }
+
+    }
+}
