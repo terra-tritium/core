8aec2723ee24b1a28cb65983c96cb34f541c4c76
Kleiton Batista <kleiton.a.batista@gmail.com>
2024-04-13 11:53:26 -0300

    ajuste pais duplicado

--- database/migrations/2024_04_13_134457_ajuste_country.php
+++ database/migrations/2024_04_13_134457_ajuste_country.php
@@ -12,18 +12,37 @@ return new class extends Migration
      */
     public function up(): void
     {
-        /*Vamos manter como o correto a primeira aparição de Switzerland*/
-        DB::statement("
-            UPDATE players 
-            SET country = (SELECT MIN(id) FROM countrys WHERE name = 'Switzerland')
-            WHERE country = (SELECT MAX(id) FROM countrys WHERE name = 'Switzerland')
-        ");
 
-        /**Vamos remover a segunda aparição de Switzerland */
-        DB::statement("
-            DELETE FROM countrys 
-            WHERE id = (SELECT MAX(id) FROM countrys WHERE name = 'Switzerland')
-        ");
+        // Primeiro, obtenha os IDs necessários
+        $minId = DB::table('countrys')->where('name', 'Switzerland')->min('id');
+        $maxId = DB::table('countrys')->where('name', 'Switzerland')->max('id');
+
+        // Somente proceda se ambos os IDs existirem e forem diferentes
+        if ($minId && $maxId && $minId !== $maxId) {
+            // Atualize os players para apontar para o ID correto
+            DB::table('players')
+                ->where('country', $maxId)
+                ->update(['country' => $minId]);
+
+            // Remova a entrada duplicada de Switzerland
+            DB::table('countrys')
+                ->where('id', $maxId)
+                ->delete();
+        }
+
+
+        //     /*Vamos manter como o correto a primeira aparição de Switzerland*/
+        //     DB::statement("
+        //         UPDATE players 
+        //         SET country = (SELECT MIN(id) FROM countrys WHERE name = 'Switzerland')
+        //         WHERE country = (SELECT MAX(id) FROM countrys WHERE name = 'Switzerland')
+        //     ");
+
+        //     /**Vamos remover a segunda aparição de Switzerland */
+        //     DB::statement("
+        //         DELETE FROM countrys 
+        //         WHERE id = (SELECT MAX(id) FROM countrys WHERE name = 'Switzerland')
+        //     ");
     }
 
     /**
