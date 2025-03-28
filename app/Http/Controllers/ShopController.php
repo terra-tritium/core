0da5a9c2852d759c6363d1a953cbc689478f1833
Andre Amorim <andre.php6@gmail.com>
2025-03-28 17:02:29 -0300

    Implement routes for shop

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -12,21 +12,35 @@ class ShopController extends Controller
         try {
             $shopService = new ShopService;
 
-            // if (!$rewardService->validNFT($wallet)) {
-            //     return response()->json("No NFT valid", Response::HTTP_BAD_REQUEST);
-            // }
+            $resultBuy = $shopService->buy($code, $planetId);
 
-            // $resultClaim = $rewardService->claim($code, $wallet, $planetId);
-            // if ($resultClaim == "ok") {
-            //     return response()->json($resultClaim, Response::HTTP_OK);
-            // } else {
-            //     return response()->json($resultClaim, Response::HTTP_BAD_REQUEST);
-            // }
+            if ($resultBuy == "ok") {
+                return response()->json($resultBuy, Response::HTTP_OK);
+            } else {
+                return response()->json($resultBuy, Response::HTTP_BAD_REQUEST);
+            }
             
         } catch (\Exception $exception) {
             Log::error($exception);
             return response()->json(['message' => 'Shop controller error'], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
+    }
 
+    public function redeem($wallet, $collection, $token_id){
+        try {
+            $shopService = new ShopService;
+
+            $resultRedeem = $shopService->redeem($wallet, $collection, $token_id);
+
+            if ($resultRedeem == "ok") {
+                return response()->json($resultRedeem, Response::HTTP_OK);
+            } else {
+                return response()->json($resultRedeem, Response::HTTP_BAD_REQUEST);
+            }
+            
+        } catch (\Exception $exception) {
+            Log::error($exception);
+            return response()->json(['message' => 'Shop controller error'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 }
